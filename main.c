56bca4346bb6c3bda1b14f9263a5dade99faeaf8
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-01-20 10:29:13 +0400

    Initial commit, pt. 2

--- main.c
+++ main.c
@@ -27,6 +27,7 @@
 #include <search.h>	// hsearch_r()
 #include <sys/types.h>	// regexec()
 #include <regex.h>	// regexec()
+#include <unistd.h>	// access()
 
 //#include "malloc.h"
 
@@ -34,43 +35,44 @@
 
 #define HT_SIZE_VSRV	(1<<8)
 
-struct hsearch_data ht_lsb_macro_default = {0};
-struct hsearch_data ht_lsb_macro         = {0};
+// macro to value
+struct hsearch_data ht_lsb_m2v = {0};
+// value to macro
+struct hsearch_data ht_lsb_v2m = {0};
 
 void syntax() {
 	fprintf(stderr, "lsb2rcconf /path/to/init/script\n");
 	exit(EINVAL);
 }
 
-/*
-static const char *lsb2orc(const char *lsb_macro) {
-	if(!strcmp(lsb_macro, "$local_fs"))
-		return "mountall mountall-bootclean umountfs";
-	else
-	if(!strcmp(lsb_macro, "$remote_fs"))
-		return "mountnfs mountnfs-bootclean umountnfs sendsigs";
-	else
-	if(!strcmp(lsb_macro, "$network"))
-		return "networking ifupdown";
-	else
-	if(!strcmp(lsb_macro, "$syslog"))
-		return "syslog";
-	else
-	if(!strcmp(lsb_macro, "$time"))
-		return "hwclock";
-	else
-	if(!strcmp(lsb_macro, "$portmap"))
-		return "rpcbind";
-}
-*/
-
-static inline void lsb2orc_add(char *key, char *data) {
+static inline void lsb_x2x_add(char *key, char *data, struct hsearch_data *ht) {
 	ENTRY entry = {key, data}, *entry_res_ptr;
+//	printf("%s: %s\n", key, data);
 
-	hsearch_r(entry, ENTER, &entry_res_ptr, &ht_lsb_macro);
+	hsearch_r(entry, ENTER, &entry_res_ptr, ht);
 }
 
-void lsb2orc_parse_insserv() {
+static inline void lsb_v2m_add(char *key, char *data) {
+	return lsb_x2x_add(key, data, &ht_lsb_v2m);
+}
+
+static inline void lsb_m2v_add(char *key, char *data) {
+	return lsb_x2x_add(key, data, &ht_lsb_m2v);
+}
+
+static const int xregcomp(regex_t *preg, const char *regex, int cflags) {
+	int r;
+	if((r=regcomp(preg, regex, cflags))) {
+		char buf[BUFSIZ];
+		regerror(r, preg, buf, BUFSIZ);
+		fprintf(stderr, "Error: Cannot compile regex: %i: %s\n", r, buf);
+		exit(r);
+	}
+
+	return r;
+}
+
+void parse_insserv() {
 	FILE *file_insserv = fopen(PATH_INSSERV, "r");
 	if(file_insserv == NULL) {
 		fprintf(stderr, "Error: Unable to read \""PATH_INSSERV"\": %i: %s\n", errno, strerror(errno));
@@ -78,13 +80,8 @@ void lsb2orc_parse_insserv() {
 	}
 
 	regex_t regex;
-	int r;
-	if((r=regcomp(&regex, "^(\\$\\S+)\\s*(\\S?.*)$", REG_EXTENDED))) {
-		char buf[BUFSIZ];
-		size_t errsize = regerror(r, &regex, buf, BUFSIZ);
-		fprintf(stderr, "Error: Cannot compile regex: %i: %s\n", r, buf);
-		exit(r);
-	}
+
+	xregcomp(&regex, "^(\\$\\S+)\\s*(\\S?.*)$", REG_EXTENDED);
 
 	size_t line_len;
 	size_t line_size = 0;
@@ -101,18 +98,29 @@ void lsb2orc_parse_insserv() {
 		line_ptr[--line_len] = 0;	// cutting-off '\n'
 
 		if(!regexec(&regex, line_ptr, 3, matches, 0)) {
-			char *key  = strdup(&line_ptr[matches[1].rm_so]);	// TODO: free() this
-			key[ matches[1].rm_eo] = 0;
-			char *data = strdup(&line_ptr[matches[2].rm_so]);	// TODO: free() this
-			data[matches[2].rm_eo] = 0;
-			lsb2orc_add(key, data);
+			char *macro    = strdup(&line_ptr[matches[1].rm_so]);	// TODO: free() this
+			macro[   matches[1].rm_eo - matches[1].rm_so] = 0;
+
+			char *services = strdup(&line_ptr[matches[2].rm_so]);	// TODO: free() this
+			services[matches[2].rm_eo - matches[2].rm_so] = 0;
+
+			// $macro:	service service service service
+			//			   services
+
+			// splitting services
+			char *strtok_saveptr = NULL;
+			char *service = strtok_r(services, " \t", &strtok_saveptr);
+			do {
+				// remembering: service => macro
+				lsb_v2m_add(service, macro);
+			} while((service = strtok_r(NULL, " \t", &strtok_saveptr)));
 		}
 	}
 
 	fclose(file_insserv);
 }
 
-void lsb2orc_init() {
+void lsb_init() {
 	ENTRY entries[] = {
 		{"$local_fs",	"mountall mountall-bootclean umountfs"},
 		{"$remote_fs",	"mountnfs mountnfs-bootclean umountnfs sendsigs"},
@@ -123,32 +131,110 @@ void lsb2orc_init() {
 		{NULL,		NULL}
 	};
 
-	hcreate_r(6,		&ht_lsb_macro_default);
-	hcreate_r(HT_SIZE_VSRV,	&ht_lsb_macro);
+	hcreate_r(6,		&ht_lsb_m2v);
+	hcreate_r(HT_SIZE_VSRV,	&ht_lsb_v2m);
 
 	ENTRY *entry_ptr = entries, *entry_res_ptr;
 	while(entry_ptr->key != NULL) {
-		hsearch_r(*entry_ptr, ENTER, &entry_res_ptr, &ht_lsb_macro_default);
+		hsearch_r(*entry_ptr, ENTER, &entry_res_ptr, &ht_lsb_m2v);
 		entry_ptr++;
 	}
 
-	lsb2orc_parse_insserv();
+	parse_insserv();
 }
 
-static const char *lsb2orc(const char *lsb_macro) {
+static const char *lsb_x2x(const char *lsb_macro, struct hsearch_data *ht) {
 	ENTRY entry, *entry_ptr;
 
 	entry.key = (char *)lsb_macro;
 
-	hsearch_r(entry, FIND, &entry_ptr, &ht_lsb_macro);
-	if(entry_ptr != NULL)
-		return entry_ptr->data;
-
-	hsearch_r(entry, FIND, &entry_ptr, &ht_lsb_macro_default);
+	hsearch_r(entry, FIND, &entry_ptr, ht);
 	if(entry_ptr != NULL)
 		return entry_ptr->data;
 
 	return NULL;
+}
+
+static const char *lsb_m2v(const char *lsb_macro) {
+	return lsb_x2x(lsb_macro, &ht_lsb_m2v);
+}
+
+static const char *lsb_v2m(const char *lsb_macro) {
+	return lsb_x2x(lsb_macro, &ht_lsb_v2m);
+}
+
+static void lsb_header_parse(const char *const header, char *value) {
+	printf("%s: %s\n", header, value);
+	return;
+}
+
+void lsb_parse(const char *initdscript) {
+	FILE *file_initdscript = fopen(initdscript, "r");
+
+	if(file_initdscript == NULL) {
+		fprintf(stderr, "Error: Unable to read \"%s\": %i: %s\n", 
+			initdscript, errno, strerror(errno));
+		exit(errno);
+	}
+
+	regex_t regex_start, regex_header, regex_end;
+
+	xregcomp(&regex_start,  "^### BEGIN INIT INFO\\s*$",     REG_EXTENDED);
+	xregcomp(&regex_header, "^#\\s*(\\S+):\\s\\s*(.*\\S)\\s*$", REG_EXTENDED);
+	xregcomp(&regex_end,    "^### END INIT INFO\\s*$",       REG_EXTENDED);
+
+	enum lsb_parse_state {
+		LP_STARTED = 0,
+		LP_PARSING_LSB = 1
+	};
+
+	enum lsb_parse_state state = LP_STARTED;
+	size_t line_len;
+	size_t line_size = 0;
+	char *line_ptr = NULL;
+	while((line_len = getline(&line_ptr, &line_size, file_initdscript))!=-1) {
+		if(!line_len)
+			continue;
+
+		line_ptr[--line_len] = 0;	// cutting-off '\n'
+
+		switch(state) {
+			case LP_STARTED: {
+				if(!regexec(&regex_start, line_ptr, 0, NULL, 0)) {
+					state = LP_PARSING_LSB;
+					continue;
+				}
+				break;
+			}
+			case LP_PARSING_LSB: {
+				regmatch_t matches[4] = {{0}};
+
+				if(!regexec(&regex_header, line_ptr, 3, matches, 0)) {
+					char *header = strdup(&line_ptr[matches[1].rm_so]);
+					header[matches[1].rm_eo - matches[1].rm_so] = 0;
+
+					char *value  = strdup(&line_ptr[matches[2].rm_so]);	// TODO: free() this
+					value[ matches[2].rm_eo - matches[2].rm_so] = 0;
+
+					lsb_header_parse(header, value);
+
+					free(header);
+				} else
+				if(!regexec(&regex_end, line_ptr, 0, NULL, 0))
+					goto l_lsb_parse_end;
+				break;
+			}
+		}
+	}
+
+l_lsb_parse_end:
+
+	fclose(file_initdscript);
+	return;
+}
+
+void lsb_print_orc() {
+	return;
 }
 
 int main(int argc, char *argv[]) {
@@ -156,9 +242,19 @@ int main(int argc, char *argv[]) {
 		syntax();
 
 	const char *initdscript = argv[1];
-	const char *name	= basename(strdup(initdscript));
+	const char *service	= basename(strdup(initdscript));
 
-	lsb2orc_init();
+	if(access(initdscript, R_OK)) {
+		fprintf(stderr, "Cannot get read access to file \"%s\": %i: %s\n",
+			initdscript, errno, strerror(errno));
+		exit(errno);
+	}
+
+	lsb_init();
+
+	lsb_parse(initdscript);
+
+	lsb_print_orc();
 
 	exit(0);
 }
